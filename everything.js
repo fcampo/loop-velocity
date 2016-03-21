e6e63afa23f472060dac0b5e51f0a5a45e971b30
Fernando Campo <fernando.campo@telefonica.com>
2016-03-21 18:36:01 +0000

    All bug and Date depending releases return same data now - Added Date.js lib for date handling - Data to print divided on [{assigned}, {solved}, {fullyDone}] with [meta] separated inside of object - Need to finish printing the info, not working at the moment

--- everything.js
+++ everything.js
@@ -15,33 +15,27 @@
  *  });
  */
 var Bugziller = (function(namespace) {
-  const QUERY_URL = "https://bugzilla.mozilla.org/rest/bug?";
+  const QUERY_URL = "https://bugzilla.mozilla.org/rest/bug?"
   var fields = [
     "id",
     "summary",
     "resolution",
     "depends_on"
   ];
-  var options = "include_fields" + fields.join(',') +
+  var options = "include_fields=" + fields.join(',') +
                 "&product=Hello%20%28Loop%29";
 
   // hashtable to avoid repetitions
   var checkedBugs = {};
 
-  // helper to add options to the query
-  // TODO: not very useful at the moment, fix or kill
-  // function getOptions() {
-  //   var result = "";
-  //   for (var a in options) {
-  //     result += "&" + a + "=" + [].concat(options[a]).join(",");
-  //   }
-  //   return result;
-  // }
-
   // checks if a specific bug is a [Meta]
   function isMeta(bug) {
-    let lower = bug.summary.toLowerCase();
-    return lower.indexOf("meta") !== -1;
+    if (!bug || !bug.summary) {
+      console.error('ooopsie no summary');
+      return false;
+    }
+    let metaReg = new RegExp(/[\[|\(]meta[\]|\)]/);
+    return metaReg.test(bug.summary);
   }
 
   // checks if a bug is closed, independently of the reason for it
@@ -96,9 +90,8 @@ var Bugziller = (function(namespace) {
     }
     return getBug(bugArray).then(infoArray => {
       infoArray.forEach(bug => {
-        //if (!isMeta(bug)) { // we could get rid of [Meta] bugs here
-          list.push(bug);
-        //}
+        // add only the id, as we'll filter later
+        list.push(bug.id);
       });
       return getDependencies(infoArray).then(depends => {
         return list.concat(depends);
@@ -112,29 +105,89 @@ var Bugziller = (function(namespace) {
   // solved:    all bugs that were solved during the release.
   //            Could have been assigned on previous release though
   // complete:  100% on time. bugs that were assigned AND solved during the release
-  function getAllFromDates(start, end) {
-    let baseQuery = QUERY_URL + options;
+  // @param start   Date starting date for the release query
+  // @param end     Date ending date for the release query
+  // @param bugList [optional] limit the search to a series of bugs
+  function getAllFromDates(start, end, bugList) {
+    let baseQuery = QUERY_URL + options +
+                    (!!bugList ? "&id=" + bugList.join(',') : "");
 
-    let commited = baseQuery +
-      '';
+    let dateIni = moment(start, "DD/MM/YYYY");
+    dateIni = (dateIni.isValid() ? dateIni : moment()).format("YYYY-MM-DD");
+    let dateEnd = moment(end, "DD/MM/YYYY");
+    dateEnd = (dateEnd.isValid() ? dateEnd : moment()).format("YYYY-MM-DD");
 
+    let qSolved = baseQuery +
+                  "&f1=resolution" +
+                  "&o1=changedafter" +
+                  "&v1=" + dateIni +
+                  "&f2=resolution" +
+                  "&o2=changedbefore" +
+                  "&v2=" + dateEnd+
+                  "&f3=resolution" +
+                  "&o3=changedto" +
+                  "&v3=fixed";
+    let qCommited = baseQuery +
+                  "&f1=assigned_to" +
+                  "&o1=changedafter" +
+                  "&v1=" + dateIni +
+                  "&f2=assigned_to" +
+                  "&o2=changedbefore" +
+                  "&v2=" + dateEnd +
+                  "&f3=assigned_to" +
+                  "&o3=changedfrom" +
+                  "&v3=nobody%40mozilla.org";
+    let qCompleted = baseQuery +
+                  "&f1=assigned_to" +
+                  "&o1=changedafter" +
+                  "&v1=" + dateIni +
+                  "&f2=assigned_to" +
+                  "&o2=changedbefore" +
+                  "&v2=" + dateEnd +
+                  "&f3=assigned_to" +
+                  "&o3=changedfrom" +
+                  "&v3=nobody%40mozilla.org" +
+                  "&f4=resolution" +
+                  "&o4=changedafter" +
+                  "&v4=" + dateIni +
+                  "&f5=resolution" +
+                  "&o5=changedbefore" +
+                  "&v5=" + dateEnd +
+                  "&f6=resolution" +
+                  "&o6=changedto" +
+                  "&v6=fixed";
+
+    return Promise.all(
+      [qSolved,
+       qCommited,
+       qCompleted].map(query => {
+      return fetch(query)
+        .then(response => response.json())
+        .then(json => json.bugs);
+      })
+    ).then(resultArray => resultArray);
   }
 
   function getReleaseData(release) {
     if (!release) {
-      console.error(' - no RELEASE to check');
+      console.error(' - no RELEASE to check -');
       return Promise.resolve();
     }
+    console.log('> Checking release ' + release.name);
     // bug dependency
     if (release.bug) {
-      return getAll(release.bug);
+      console.log('-- from bug');
+      return getAll(release.bug).then(list => {
+        return getAllFromDates(release.start, release.end, list);
+      });
     }
+    console.log('-- from dates');
     // date dependency
     return getAllFromDates(release.start, release.end);
   }
 
   return {
-    getRel: getReleaseData,
+    getRelease: getReleaseData,
     getAll: getAll,
     getOne: getBug,
     isMeta: isMeta,
@@ -368,58 +421,83 @@ const RELEASES = [
   }
 ];
 
-
 RELEASES.reduce((sequence, release) => {
   // console.log('...inside reduce');
   // console.log(release);
   return sequence.then(() => {
     // console.log('...inside sequence.then');
-    return Bugziller.getRel(release);
-  }).then(bugList => {
+    return Bugziller.getRelease(release);
+  }).then(fullBugList => {
     // console.log('bugList = ' + JSON.stringify(bugList));
-    return filterBugs(bugList);
+    return filterBugs(fullBugList);
   }).then(classifiedLists => {
-    // console.log('filtered = ' + JSON.stringify(classifiedLists));
+    console.log('filtered = ' + JSON.stringify(classifiedLists));
     return printData(release, classifiedLists);
   });
 }, Promise.resolve())
-.catch((error) => console.error('something went WRONG'))
+.catch((error) => console.error('something went WRONG ' + error))
 .then(() => UI.loading.classList.add('nope'));
 
-function filterBugs(list) {
-  if (!list) {
+/**
+ * Cleans the bug lists from not desired values.
+ * In this case, [Meta] bugs
+ * @param filteredRelease 3-pos-array with bug array inside.
+ *        [0] - all assigned bugs for the release.
+ *        [1] - all solved bugs during the release.
+ *        [2] - bugs both assigned and solved during release.
+ */
+function filterBugs(filteredRelease) {
+  if (!filteredRelease || filteredRelease.length != 3) {
+    console.error('Wrongly filtered list: ' + filteredRelease);
     return Promise.resolve();
   }
 
-  let total = list.length,
+  let total = filteredRelease.length,
       metas = [],
       taken = [],
       fixed = [];
 
-  list.forEach(bug => {
-    if (Bugziller.isMeta(bug)) {
-      metas.push(bug);
-    } else {
-      // counted for resolution
-      taken.push(bug);
+  // [[assigned], [solved], [fullyDone]]
+  return Promise.resolve(filteredRelease.map(bugList => {
+    let metas = [];
+    let done = [];
 
-      if (Bugziller.isClosed(bug)) {
-        fixed.push(bug);
+    bugList.forEach((bug, pos) => {
+      console.log('checking bug ' + JSON.stringify(bug) + '-' + pos);
+      if (Bugziller.isMeta(bug)) {
+        metas.push(bug);
+        bugList.splice(pos, 1);
+      // } else {
+      //   // counted for resolution
+      //   done.push(bug);
       }
-    }
-  });
-  return Promise.resolve({
-    total: total,
-    metas: metas,
-    taken: taken,
-    fixed: fixed
-  });
+    });
+
+    return {
+      metas: metas,
+      done: bugList
+    };
+  }));
+
+  // return Promise.resolve(filteredRelease);
+  // {
+  //   total: total,
+  //   metas: metas,
+  //   taken: taken,
+  //   fixed: fixed
+  // });
 }
 
+// data = [
+//  [assigned = { metas, done }],
+//  [solved = { metas, done }],
+//  [fullyDone = { metas, done }]
+// ]
 function printData(origin, data) {
   if (!origin || !data) {
     return Promise.resolve();
   }
+
   // fill data
   let newRelease = document.createElement('li');
   let name = document.createElement('h2');
@@ -454,96 +532,96 @@ function printData(origin, data) {
 }
 
 // ------------------------
-// for (let rel in RELEASES) {
-//   // show spinner
+  // for (let rel in RELEASES) {
+  //   // show spinner
 
-//   // query
-//   Bugziller.getRel(RELEASES[rel])
-//   // paint results
-//   .then(fullList => {
-//     if (!fullList) {
-//       return;
-//     }
-//     var totalBugs = fullList.length;
-//     fullList.forEach(bug => {
-//       if (Bugziller.isMeta(bug)) {
-//         metaBugs.push(bug);
-//       } else {
-//         // counted for resolution
-//         commitedBugs.push(bug);
+  //   // query
+  //   Bugziller.getRelease(RELEASES[rel])
+  //   // paint results
+  //   .then(fullList => {
+  //     if (!fullList) {
+  //       return;
+  //     }
+  //     var totalBugs = fullList.length;
+  //     fullList.forEach(bug => {
+  //       if (Bugziller.isMeta(bug)) {
+  //         metaBugs.push(bug);
+  //       } else {
+  //         // counted for resolution
+  //         commitedBugs.push(bug);
 
-//         if (Bugziller.isClosed(bug)) {
-//           closedBugs.push(bug);
-//         }
-//       }
-//     });
-//     // fill data
-//     let newRelease = document.createElement('li');
-//     let name = document.createElement('h2');
-//     name.textContent = 'v' + rel + ' | ' +
-//                        '(' + rel.start + ' - ' + rel.end + ')';
-//     newRelease.appendChild(name);
+  //         if (Bugziller.isClosed(bug)) {
+  //           closedBugs.push(bug);
+  //         }
+  //       }
+  //     });
+  //     // fill data
+  //     let newRelease = document.createElement('li');
+  //     let name = document.createElement('h2');
+  //     name.textContent = 'v' + rel + ' | ' +
+  //                        '(' + rel.start + ' - ' + rel.end + ')';
+  //     newRelease.appendChild(name);
 
-//     let total = document.createElement('h3');
-//     total.textContent = totalBugs + ' bugs in total';
-//     newRelease.appendChild(total);
+  //     let total = document.createElement('h3');
+  //     total.textContent = totalBugs + ' bugs in total';
+  //     newRelease.appendChild(total);
 
-//     let meta = document.createElement('h3');
-//     meta.textContent = 'including ' + metaBugs.length + ' [Meta] bugs';
-//     newRelease.appendChild(meta);
+  //     let meta = document.createElement('h3');
+  //     meta.textContent = 'including ' + metaBugs.length + ' [Meta] bugs';
+  //     newRelease.appendChild(meta);
 
-//     let commited = document.createElement('h3');
-//     commited.textContent = 'From ' + commitedBugs.length + ' commited';
-//     newRelease.appendChild(commited);
+  //     let commited = document.createElement('h3');
+  //     commited.textContent = 'From ' + commitedBugs.length + ' commited';
+  //     newRelease.appendChild(commited);
 
-//     let fixed = document.createElement('h3');
-//     fixed.textContent = closedBugs.length + ' were solved';
-//     newRelease.appendChild(fixed);
+  //     let fixed = document.createElement('h3');
+  //     fixed.textContent = closedBugs.length + ' were solved';
+  //     newRelease.appendChild(fixed);
 
-//     UI.results.appendChild(newRelease);
+  //     UI.results.appendChild(newRelease);
 
-//     console.log('RELEASE - ' + rel);
-//     console.log('total - ' + totalBugs);
-//     console.log('[meta] - ' + metaBugs.length);
-//     console.log('commited - ' + commitedBugs.length);
-//     console.log('solved - ' + closedBugs.length);
-//     console.log();
+  //     console.log('RELEASE - ' + rel);
+  //     console.log('total - ' + totalBugs);
+  //     console.log('[meta] - ' + metaBugs.length);
+  //     console.log('commited - ' + commitedBugs.length);
+  //     console.log('solved - ' + closedBugs.length);
+  //     console.log();
 
-//     return Promise.resolve();
-//   });
-// }
-//   // show it
-//   UI.loading.classList.add('nope');
-// ------------------------
-// Bugziller.getAll(1248602).then(fullList => {
-//   var total = fullList.length;
-//   fullList.forEach(bug => {
-//     if (Bugziller.isMeta(bug)) {
-//       metaBugs.push(bug);
-//     } else {
-//       // counted for resolution
-//       commitedBugs.push(bug);
+  //     return Promise.resolve();
+  //   });
+  // }
+  //   // show it
+  //   UI.loading.classList.add('nope');
+  // ------------------------
+  // Bugziller.getAll(1248602).then(fullList => {
+  //   var total = fullList.length;
+  //   fullList.forEach(bug => {
+  //     if (Bugziller.isMeta(bug)) {
+  //       metaBugs.push(bug);
+  //     } else {
+  //       // counted for resolution
+  //       commitedBugs.push(bug);
 
-//       if (Bugziller.isClosed(bug)) {
-//         closedBugs.push(bug);
-//       }
-//     }
-//   });
-//   // fill data
-//   UI.totalBugs.textContent = total;
-//   UI.metaBugs.textContent = metaBugs.length;
-//   UI.assignedBugs.textContent = commitedBugs.length;
-//   UI.fixedBugs.textContent = closedBugs.length;
+  //       if (Bugziller.isClosed(bug)) {
+  //         closedBugs.push(bug);
+  //       }
+  //     }
+  //   });
+  //   // fill data
+  //   UI.totalBugs.textContent = total;
+  //   UI.metaBugs.textContent = metaBugs.length;
+  //   UI.assignedBugs.textContent = commitedBugs.length;
+  //   UI.fixedBugs.textContent = closedBugs.length;
 
-//   // show it
-//   UI.results.classList.remove('nope');
-//   UI.loading.classList.add('nope');
+  //   // show it
+  //   UI.results.classList.remove('nope');
+  //   UI.loading.classList.add('nope');
 
-//   console.log('total - ' + total);
-//   console.log('[meta] - ' + metaBugs.length);
-//   console.log('commited - ' + commitedBugs.length);
-//   console.log('solved - ' + closedBugs.length);
-// });
+  //   console.log('total - ' + total);
+  //   console.log('[meta] - ' + metaBugs.length);
+  //   console.log('commited - ' + commitedBugs.length);
+  //   console.log('solved - ' + closedBugs.length);
+  // });
 
 // META   1248602
 // NON-F  1248604
